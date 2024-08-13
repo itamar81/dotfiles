582ebafdff15cc0085a7f2772252adae6aa6c93d
itamarh <itamarh@nvidia.com>
2024-08-13 12:40:22 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
-snap_apps="go kubectl kubectx terraform vault yq helm starship"
+snap_apps="go kubectl kubectx terraform vault yq helm"
 for app in $snap_apps;
 do
     snap install $app --classic
 done
+snap install starship --classic --edge
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
 mkdir $HOME/.config
 dir_name=$(dirname "$0")
 cp $dir_name/.config/starship $HOME/.config
