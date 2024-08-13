ebae4d67cc1ffaea1e283057a87f4c0943f20c28
itamarh <itamarh@nvidia.com>
2024-08-13 15:21:02 +0300

    fix

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ tar zxvf "${KREW}.tar.gz"
 wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/openshift-client-linux.tar.gz
 tar -xvf openshift-client-linux.tar.gz
 cp oc /usr/local/bin/
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export PATH="${KREW_ROOT:-/.krew}/bin:$PATH"
 KREW_PLUGINS="access-matrix allctx cert-manager creyaml ctx deprecations df-pv eksporter exec-cronjob grep konfig ns rabbitmq split-yaml starboard"
 KREW_PLUGINS="${KREW_PLUGINS} support-bundle tree unused-volumes  view-cert view-serviceaccount-kubeconfig  view-secret  who-can whoami rolesum  resource-versions"
 KREW_PLUGINS="${KREW_PLUGINS} outdated node-shell neat get-all mc ipick minio virt example"
