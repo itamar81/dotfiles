1a1b210080f096f5db5538985e19f85e629e3bd2
itamarh <itamarh@nvidia.com>
2024-08-13 11:17:12 +0300

    fix

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ for app in $snap_apps;
 do
     echo "sudo snap install $app --classic"
 done
-# mkdir configs
+mkdir $HOME/.config
+cp dotfiles/.config/starship $HOME/.config
+
 cd ..
+
 git clone https://github.com/jonmosco/kube-ps1
 cp kube-ps1/kube-ps1.sh /usr/local/etc/
